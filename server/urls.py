0ab71195226e8db78b4910da9a7a71097f68515f
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-05-11 16:39:54 -0300

    add server app urls.py

---
+++ server/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('server.views',
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^server/', include('server.urls')),
+)
