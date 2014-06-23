cbf55e8d3a08c8c61ffe43c056e9fdf5e06da005
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-06-22 23:48:47 -0300

    moved signing of data to json_response function

--- server/views.py
+++ server/views.py
@@ -23,99 +23,59 @@ from .models.destination.BaseDestination import BaseDestination
 def origin_available(request):
     if not authenticated(None, request.GET.dict()):
         return HttpResponseForbidden()
-    oname = request.GET.get(u"origin", None)
-    print oname
-    if not oname:
+    origin_name = request.GET.get(u"origin", None)
+    if not origin_name:
         return HttpResponseBadRequest(u"<h1>valor inválido</h1>")
+
+    respond = lambda available: json_response({u"availability":available})
+    origin = Origin.objects.filter(name=origin_name)
+    return respond(False) if origin.exists() else respond(True)
     
-    respond = lambda available: \
-                json_response(
-                    get_signed_data({u"availability":available},
-                    settings.R_SIGNATURE_KEY)
-                )
-    
-    origin = Origin.objects.filter(name=oname)
-    print origin
-    #print respond(False)
-    #print respond(True)
-    print respond(False) if origin else respond(True)
-    return respond(False) if origin else respond(True)
-    
-#@require_POST
+@require_POST
 @csrf_exempt
 def register_origin(request):
     #import ipdb; ipdb.set_trace()
-    if request.method == "POST":
-        if not authenticated(None, request.POST.dict()):
-            return HttpResponseForbidden()
-        oname = request.POST.get(u"origin", None)
-        print oname
-        if not oname:
-            return HttpResponseBadRequest(u"<h1>valor inválido</h1>")
-        
-        origin, created = Origin.objects.get_or_create(
-            name=oname
-        )
-        print origin, created
-        if not created:
-            return HttpResponseBadRequest(u"<h1>Origem já está cadastrada</h1>")
-        
-        return json_response(
-            get_signed_data(
-                {u"origin": origin.name,
-                 u"id"    : origin.id,
-                 u"apikey": unicode(origin.apikey),
-                },
-                settings.R_SIGNATURE_KEY
-            )
-        )
-    else:
-        return HttpResponseNotFound(u"<h1>Não há nada aqui @_@</h1>")
+    #if request.method == "POST":
+    if not authenticated(None, request.POST.dict()):
+        return HttpResponseForbidden()
+    origin_name = request.POST.get(u"origin", None)
+    if not origin_name: return HttpResponseBadRequest(u"<h1>valor inválido</h1>")
+    
+    origin = Origin.objects.create(name=origin_name)
+    return json_response({ u"origin": origin.name
+                         ,  u"id"    : origin.id
+                         ,  u"apikey": unicode(origin.apikey)
+                         })
+    #else:
+    #    return HttpResponseNotFound(u"<h1>Não há nada aqui @_@</h1>")
 
 
 @require_GET
 def retrieve_destinations(request, id):
     #if request.method == 'GET':
         
-        if not id:
-            import ipdb; ipdb.set_trace()
-            return HttpResponseForbidden()
-        elif not authenticated(id, request.GET.dict()):
-            import ipdb; ipdb.set_trace()
-            return HttpResponseForbidden()
+    if not id:
+        import ipdb; ipdb.set_trace()
+        return HttpResponseForbidden()
+    elif not authenticated(id, request.GET.dict()):
+        import ipdb; ipdb.set_trace()
+        return HttpResponseForbidden()
+    
+    return json_response( {u"destinations": [d.name for d in
+                                               BaseDestination.objects.all()]}
+                        , Origin.objects.get(id=id).apikey )
 
-        return json_response(
-            get_signed_data(
-                {u"destinations": [d.name for d in BaseDestination.objects.all()]},
-                Origin.objects.get(id=id).apikey
-            )
-        )
     #else:
     #    return HttpResponseNotFound()
 
 def authenticated(id, fulldata):
     signature = fulldata.get(u"signature", None)
     data = remove_key(fulldata, u"signature")
-    #import ipdb; ipdb.set_trace()
-    
     origin = Origin.objects.filter(id=id)
-    if origin:
-        import ipdb; ipdb.set_trace()
-        print id
-        print signature
-        print origin[0]
-        print origin[0].apikey
+    apikey = origin[0].apikey if origin.exists() else None
+    return False if not signature else signature == sign(data, apikey)
     
-    if not signature:
-        return False
-    try:
-        return signature == sign(data, Origin.objects.get(id=id).apikey)
-    except Origin.DoesNotExist:
-        if id:
-            return False
-        return signature == sign(data, settings.R_SIGNATURE_KEY)
-    
-def sign(data, key):
+def sign(data, apikey=None):
     sha1 = SHA.new()
     if not data:
         sha1.update(u"None")
@@ -123,17 +83,15 @@ def sign(data, key):
         sorted_data = sorted(data.iteritems(),key=operator.itemgetter(0))
         for item in sorted_data:
             sha1.update(unicode(item))
-    sha1.update(key)
+    used_key = apikey or settings.R_SIGNATURE_KEY
+    sha1.update(used_key)
     
-    #import ipdb; ipdb.set_trace()
     return sha1.hexdigest()
 
-def get_signed_data(data, key):
+def get_signed_data(data, apikey):
     return_data = dict(data) if data else {}
-    #import ipdb; ipdb.set_trace()
     return_data[u"timestamp"] = u"%s" % json.dumps(unicode(datetime.now()))
-    return_data[u"signature"] = u"%s" % sign(return_data, key)
-    #import ipdb; ipdb.set_trace()
+    return_data[u"signature"] = u"%s" % sign(return_data, apikey)
     return return_data
 
 def remove_key(d, key):
@@ -141,40 +99,40 @@ def remove_key(d, key):
     del r[key]
     return r
 
-#@require_POST
+@require_POST
 @csrf_exempt
 def backup(request, id):
-    if request.method == 'POST':
-        #return json_response([request.POST, request.FILES])
-        #values = request.POST.get('values', None)
-        #if not values:
-        #    print u'<h1>valor inválido</h1>'
-        #    return HttpResponseBadRequest(u'<h1>valor inválido</h1>')
-        
-        django_file = request.FILES.get('file', None)
-        if not django_file:
-            print u'<h1>nenhum arquivo recebido</h1>'
-            return HttpResponseBadRequest(u'<h1>nenhum arquivo recebido</h1>')
-        
-        #if values['sha1sum'] != file_sha_checksum(django_file):
-        #    print u'<h1>arquivo corrompido</h1>'
-        #    return HttpResponseBadRequest(u'<h1>arquivo corrompido</h1>')
-        
-        to_bool = lambda b: bool(b) and b.lower() not in ('false', '0')
-        return json_response(
-            backup_file(
-                django_file,
-                request.POST.get('origin', None),#values['origin'],
-                request.POST.get('destination', None),#values['destination'],
-                request.POST.get('date', None), #values['date'],
-                request.POST.get('sha1sum', None),#None, #values['sha1sum'],
-                to_bool(request.POST.get('before_restore', None)),#values['before_restore'],
-                to_bool(request.POST.get('after_restore', None)),#values['after_restore'],
-            )
+    #if request.method == 'POST':
+    #return json_response([request.POST, request.FILES])
+    #values = request.POST.get('values', None)
+    #if not values:
+    #    print u'<h1>valor inválido</h1>'
+    #    return HttpResponseBadRequest(u'<h1>valor inválido</h1>')
+    
+    django_file = request.FILES.get('file', None)
+    if not django_file:
+        print u'<h1>nenhum arquivo recebido</h1>'
+        return HttpResponseBadRequest(u'<h1>nenhum arquivo recebido</h1>')
+    
+    #if values['sha1sum'] != file_sha_checksum(django_file):
+    #    print u'<h1>arquivo corrompido</h1>'
+    #    return HttpResponseBadRequest(u'<h1>arquivo corrompido</h1>')
+    
+    to_bool = lambda b: bool(b) and b.lower() not in ('false', '0')
+    return json_response(
+        backup_file(
+            django_file,
+            request.POST.get('origin', None),#values['origin'],
+            request.POST.get('destination', None),#values['destination'],
+            request.POST.get('date', None), #values['date'],
+            request.POST.get('sha1sum', None),#None, #values['sha1sum'],
+            to_bool(request.POST.get('before_restore', None)),#values['before_restore'],
+            to_bool(request.POST.get('after_restore', None)),#values['after_restore'],
         )
-    else:
-        print u'<h1>não há nada aqui @_@</h1>'
-        return HttpResponseNotFound(u'<h1>não há nada aqui @_@</h1>')
+    )
+    #else:
+    #    print u'<h1>não há nada aqui @_@</h1>'
+    #    return HttpResponseNotFound(u'<h1>não há nada aqui @_@</h1>')
 
 #@require_POST
 @csrf_exempt
@@ -195,11 +153,9 @@ def restore(request, id):
 def message(request, id):
     pass
 
-def json_response(data):
-    return HttpResponse(
-        json.dumps(data),
-        content_type='application/json; charset=utf-8'
-    )
+def json_response(data, apikey=None):
+    return HttpResponse( json.dumps(get_signed_data(data, apikey)),
+                         content_type='application/json; charset=utf-8' )
 
 
 def file_sha_checksum(django_file):
