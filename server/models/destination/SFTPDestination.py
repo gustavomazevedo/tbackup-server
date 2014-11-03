fa4984a20c65117d32689338659022fdda580093
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-11-03 21:07:27 -0200

    Implemented SFTP recover, for backup recovery

--- server/models/destination/SFTPDestination.py
+++ server/models/destination/SFTPDestination.py
@@ -12,30 +12,25 @@ from ..mixins     import AccessableMixin
 
 class SFTPDestination(BaseDestination, AccessableMixin):
     
-    def backup(sclienelf, contents, subdir, filename, *args, **kwargs):
-        print "Hello! This is %s's backup method" % self.__class__.__name__
-
-        fd = os.path.join(self.directory, subdir)
-        
-        #transport = paramiko.Transport((self.hostname, self.port))
-        #transport.connect(username=self.username,
-        #                  pkey=paramiko.RSAKey.from_private_key(rsa_private_key))
-        #sftp = paramiko.SFTPClient.from_transport(transport)
-        #
+    def connect(self):
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.load_system_host_keys()
         pvtkey = paramiko.RSAKey.from_private_key_file(self.key_filename)
-        print (self.hostname,
-               int(self.port),
-               self.username,
-               pvtkey
-               )
+        
         client.connect(hostname=self.hostname,
                        port=int(self.port),
                        username=self.username,
                        pkey=pvtkey,
                        timeout=5.0)
+        return client
+    
+    def backup(self, contents, subdir, filename, *args, **kwargs):
+        print "Hello! This is %s's backup method" % self.__class__.__name__
+
+        fd = os.path.join(self.directory, subdir)
+        client = self.connect()
+        
         try: 
             sftp = client.open_sftp()
             
@@ -44,10 +39,8 @@ class SFTPDestination(BaseDestination, AccessableMixin):
                 logging.warning('caminho criado')
             
             sftp.chdir(subdir)
-            #fn = os.path.join(fd, filename)
-            fn = filename
             try:
-                with sftp.open(fn, 'wb+') as f:
+                with sftp.open(filename, 'wb+') as f:
                     for data in contents.chunks():
                         f.write(data)
             except Exception, e:
@@ -59,17 +52,22 @@ class SFTPDestination(BaseDestination, AccessableMixin):
             sftp.close()
             client.close()
             raise
+
     
     def restore(self, contents, subdir, filename, *args, **kwargs):
         print "Hello! This is %s's restore method" % self.__class__.__name__
-        fn = os.path.join(self.directory,
-                          subdir,
-                          filename)
+        
+        client = self.connect()
+
         try:
-            with open(fn, 'rb') as f:
-                return ((data for data in iter(lambda: f.read(64 * 1024), '')), True)
+            sftp = client.open_sftp()
+            sftp.chdir(subdir)
+                
+            with sftp.open(filename, 'rb') as f:
+                return ([chunks for chunks in iter(lambda: f.read(64 * 1024), '')], True)
         except Exception, e:
             print e
+            logging.error(e, errno)
             return None, False
     
     def _rexists(self, sftp, path):
