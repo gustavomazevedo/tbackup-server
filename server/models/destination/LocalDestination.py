8366d81643a2915ed97a6ca26311963585dc83c8
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2014-11-03 21:05:59 -0200

    Exchanged generator to list of chunks in backup retrieval

--- server/models/destination/LocalDestination.py
+++ server/models/destination/LocalDestination.py
@@ -12,11 +12,10 @@ class LocalDestination(BaseDestination):
         print "Hello! This is %s's backup method" % self.__class__.__name__
         fd = os.path.join(os.path.expanduser(self.directory), subdir)
         if not os.path.exists(fd):
-            print os.path.exists(fd)
-            print 'criando caminho'
             os.makedirs(fd)
+            print 'caminho ' + fd + ' criado'
+            
         fn = os.path.join(fd,filename)
-        print fn
         try:
             with open(fn, 'wb+') as f:
                 for data in iter(lambda: contents.read(64 * 1024), ''):
@@ -33,7 +32,7 @@ class LocalDestination(BaseDestination):
                           filename)
         try:
             with open(fn, 'rb') as f:
-                return ((data for data in iter(lambda: f.read(64 * 1024), '')), True)
+                return ([data for data in iter(lambda: f.read(64 * 1024), '')], True)
         except Exception, e:
             print e
             return None, False
