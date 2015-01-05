d6de08740b5e004d51a732d481ba49c36e3a038e
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2015-01-05 19:26:47 -0200

    changing the line of the print

--- server/tests.py
+++ server/tests.py
@@ -51,8 +51,8 @@ def rm_dir_files(dirname):
                 print 'remove file %s' % file_path
                 os.remove(file_path)
             elif os.path.isdir(file_path):
-                print 'remove dir %s' % file_path
                 rm_dir_files(file_path)
+                print 'remove dir %s' % file_path
                 shutil.rmtree(file_path)
         except Exception, e:
             print 'ERROR %s' % e
